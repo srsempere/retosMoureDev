2ff25f01fc29d0bc3b0bedcde13666ffe7a4d9da
Samu <samueljose.garcia@iesdonana.org>
2022-12-28 20:23:23 +0100

    Implementación de fizzbuzz en java

---
+++ Java/fizzbuzz.java
@@ -0,0 +1,23 @@
+package Java;
+
+/**
+ * fizzBuzz
+ */
+public class fizzbuzz {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            boolean divTres = i % 3 == 0;
+            boolean divCinco = i % 5 == 0;
+            if (divTres && divCinco) {
+                System.out.println("fizzbuz");
+            } else if (divTres) {
+                System.out.println("fizz");
+            } else if (divCinco) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
