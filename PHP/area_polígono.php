f783dda60e0c40660984242a34e0d8a03def2e46
Samu <samueljose.garcia@iesdonana.org>
2022-12-29 17:35:19 +0100

    TODO: área de un polígono.

---
+++ PHP/area_polígono.php
@@ -0,0 +1,19 @@
+<?php
+// Calcular el área de un polígono.
+
+
+/**
+ * Triángulo:
+ * Área = (base * altura) / 2
+ *
+ * Cuadrado:
+ * Área = lado^2 (lado x lado)
+ *
+ * Rectángulo:
+ * Área = largo * ancho (base x altura)
+ */
+
+function area_poligono($)
+{
+
+}
