1aacde4dc6a209bd0e10bc1dff7652b2c0abb5c4
Samuel García <samueljose.garcia@iesdonana.org>
2023-09-29 12:18:41 +0200

    Cuenta palabras. Funcionando

--- PHP/cuentapalabras.php
+++ PHP/cuentapalabras.php
@@ -7,5 +7,40 @@
  - No se pueden utilizar funciones propias del lenguaje que lo resuelvan automáticamente.
 */
 
-$texto = file_get_contents('texto.txt');
-echo $texto;
+$cad_prueba = file_get_contents('texto.txt');
+
+
+// LIMPIADO DE TEXTO.
+$texto = strtolower($cad_prueba);
+
+$texto = preg_replace('/[.,;:¡!?"\'-]/', '', $texto);
+
+// Reemplazar todos los caracteres de espacio en blanco (espacios, saltos de línea, etc.) con un espacio
+$texto = preg_replace('/\s+/', ' ', $texto);
+
+// DIVISÓN DEL TEXTO
+$array_cadena = explode(' ', trim($texto));
+
+$array_ocurrencias = [];
+
+
+// Recorro el array para comprobar si esa palabra ha salido antes y está dentro de $array ocurrencias
+foreach ($array_cadena as $palabra) {
+    if (isset($array_ocurrencias[$palabra])) {
+        $array_ocurrencias[$palabra]++;
+    } else {
+        $array_ocurrencias[$palabra] = 1;
+    }
+}
+?>
+
+<ul>
+
+<?php
+foreach ($array_ocurrencias as $palabra => $ocurrencia) {
+?>
+    <li><?= $palabra . ': ' . $ocurrencia . ' veces aparece.'?></li>
+<?php
+} ?>
+</ul>
+<?php
